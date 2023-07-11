4a90867f82939d9b976fe6be6da496931a20cc3a
May <maypham@utexas.edu>
2023-07-10 22:18:34 -0500

    added code to apply css class to current hour

--- Assets/script.js
+++ Assets/script.js
@@ -2,35 +2,29 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
 
-  // save click function to local storage
-  // key = hour defined in div id 
-  // value = text description input by user
+  // TODO: Add a listener for click events on the save button.
+  // local storage for hour and text input value
   $('.btn').on('click', function(){
     var key = $(this).parent().attr('id').split('-')[1];
     var value = $(this).parent().find('.description').val();
     localStorage.setItem(key, value);
   })
   });
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
-  // defined current hour by day js and compared to block hour defined by div id
+  // define variables for current hour using (dayjs) and block hour using (this)
   var currentHour = dayjs().format('H');
-
   $(".time-block").each(function() {
-  var blockHour = $(this).attr('id').split('-')[1];
-
+    var blockHour = $(this).attr('id').split('-')[1];
+    
+  // TODO: Add code to get any user input that was saved in localStorage
+  // define variables to get input text entry from local storage
+    var textEntry = localStorage.getItem(blockHour);
+    var textArea = $(this).find('.description');
+    textArea.val(textEntry); 
+    
+  // TODO: Add code to apply the past, present, or future class to each time
+  // compare blockhour and current hour from dayjs to apply css per hour
     if (blockHour < currentHour) {
       $(this).find('.description').addClass('past');
     } else if (blockHour === currentHour) {
@@ -40,13 +34,8 @@ $(function () {
     };
   })
   
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   // TODO: Add code to display the current date in the header of the page.
+  // display date using dayjs
   var today = dayjs();
   $('#currentDay').text(today.format('dddd, MMMM D h:mm a'));
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Work-Day-Scheduler-
 Challenge 05
+
+## Description
+This assignment was created to showcase the skills of May Pham to incorporate HTML, JQuery, and CSS to create a daily planner that displays the current date, saves information to local storage, and color coordinates the time based on current, future, and past times. Some methods of JQuery demonstrated in this activity include dayjs, local files, this, if else statements, etc. 
+
+## Usage
+![Webpage Screenshot](./Assets/screencapture.png)
+
+https://github.com/mayphamx/Work-Day-Scheduler-
+
+https://mayphamx.github.io/Work-Day-Scheduler-/ 
